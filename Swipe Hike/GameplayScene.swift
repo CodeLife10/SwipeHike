882a39b5e44fe52ce5350450aeab404beb4d1cbd
Craigieboy10 <craig-watt@live.co.uk>
2017-05-08 17:57:29 +0100

    player exists and moves left to right, still requires animation and some sort of collision detection refinement

--- Swipe Hike/GameplayScene.swift
+++ Swipe Hike/GameplayScene.swift
@@ -22,6 +22,16 @@ class GameplayScene: SKScene {
     private var rgnd2: GNDClass?
     private var rgnd3: GNDClass?
     
+    private var player: Player?
+    
+    var constantSpeed:Int = 600
+    
+    var lrrl:Int = 0;
+    //var jumpAction:SKAction = SKAction.rotate(byAngle:-3, duration:5);
+    //var jumpToLeft:SKAction = SKAction.rotate(byAngle:-9, duration:3);
+    //var jumpToRight:SKAction = SKAction.rotate(byAngle:9, duration:3);
+    //var jumpAction:SKAction = SKAction.rotate
+    
     private var mainCamera: SKCameraNode?;
     
     override func didMove(to view: SKView) {
@@ -29,10 +39,33 @@ class GameplayScene: SKScene {
         
     }
     override func update(_ currentTime: TimeInterval) {
+        //maintain constant speed?
+        player!.physicsBody?.velocity = CGVector(dx: constantSpeed, dy: 0)
         manageCamera()
         manageBGandG()
+        managePlayer()
+    }
+    
+    //user input
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //reverseGravity()
+        reverseSpeed()
+        
+        //decide which way to rotate?
+       // if lrrl == 0{
+       //     player?.run(jumpToRight)
+       //     lrrl = 1
+       // }
+       // else{
+       //     player?.run(jumpToLeft)
+       //     lrrl = 0
+       // }
+        
     }
     private func initializeGame(){
+        //set gravity to zero?
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+    
         mainCamera = childNode(withName: "MainCamera") as? SKCameraNode!
         
         bg1 = childNode(withName: "BG1") as? BGClass!
@@ -43,9 +76,20 @@ class GameplayScene: SKScene {
         gnd2 = childNode(withName: "GND2") as? GNDClass!
         gnd3 = childNode(withName: "GND3") as? GNDClass!
         
+        gnd1?.initializeGND()
+        gnd2?.initializeGND()
+        gnd3?.initializeGND()
+        
         rgnd1 = childNode(withName: "rGND1") as? GNDClass!
         rgnd2 = childNode(withName: "rGND2") as? GNDClass!
         rgnd3 = childNode(withName: "rGND3") as? GNDClass!
+        
+        rgnd1?.initializeGND()
+        rgnd2?.initializeGND()
+        rgnd3?.initializeGND()
+        
+        player = childNode(withName: "Player") as? Player!
+        player?.initializePlayer()
         
     }
     
@@ -53,6 +97,9 @@ class GameplayScene: SKScene {
         self.mainCamera?.position.y += 2.5
     }
     
+    private func managePlayer(){
+            player?.move()
+    }
     private func manageBGandG(){
         bg1?.moveBG(camera: mainCamera!)
         bg2?.moveBG(camera: mainCamera!)
@@ -65,39 +112,14 @@ class GameplayScene: SKScene {
         rgnd1?.moveGND(camera: mainCamera!)
         rgnd2?.moveGND(camera: mainCamera!)
         rgnd3?.moveGND(camera: mainCamera!)
-        
+    }
+    
+    private func reverseGravity(){
+        physicsWorld.gravity.dx *= -1
+        //player?.reversePlayer();
+    }
+    
+    private func reverseSpeed(){
+        constantSpeed *= -1;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
